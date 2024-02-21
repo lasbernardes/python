a110cb84ccb5a9ec06a2fe1cb8216a4443d371ab
Luciano Bernardes <142942523+lasbernardes@users.noreply.github.com>
2024-02-21 16:25:17 -0300

    meu1 tkinter frame

---
+++ meu1tkinter_pack_frame.py
@@ -0,0 +1,92 @@
+import tkinter as tk
+
+def greeting():
+    print('Hello stdout world!...')
+
+def clean_func(e2,e1):
+    e2.delete(0, tk.END)
+    e1.delete(0, tk.END)
+    print('Cleaned...')
+
+def reply_func(e2,e1):
+    e1.delete(0, tk.END)
+    e1.insert(0, e2.get())
+    print('Replyed...')
+
+def quit_func(b3):
+    print('Quited...')
+    b3.quit()
+
+
+def main():
+
+    root = tk.Tk()   
+    root.title('Reply - Clean - Quit')
+    root.geometry("800x600")
+
+    frame1a = tk.Frame()
+    frame1a = tk.Frame(root, width=300, height=200, borderwidth=10, padx=10, pady=10, relief="sunken")
+    frame1a.pack()
+    frame1a.place(x=0,y=0)
+
+    frame1b = tk.Frame()
+    frame1b = tk.Frame(root, width=300, height=200, borderwidth=10, padx=10, pady=10, relief="sunken")
+    frame1b.pack()
+    frame1b.place(x=400, y=0)
+
+    frame2 = tk.Frame()
+    frame2 = tk.Frame(root, width=800, height=200, borderwidth=10, padx=10, pady=10, relief="sunken")
+    frame2.pack()
+    frame2.place(y=200)
+
+    frame3 = tk.Frame()
+    frame3 = tk.Frame(root, width=800, height=200, borderwidth=10, padx=10, pady=10, relief="sunken")
+    frame3.pack()
+    frame3.place(y=400)
+
+    
+    e1 = tk.Entry(frame1a)
+    #e1.pack(side="left", fill="x", expand=True)
+    e1.place(anchor="center")
+    e2 = tk.Entry(frame1b)
+    #e2.pack(side="right", fill="x", expand=True)
+    e2.place(anchor="center")
+
+    b1 = tk.Button(frame2, text='Reply', command=lambda: reply_func(e2,e1))
+    b1.pack(side="left", fill="both", expand=True) 
+    b2 = tk.Button(frame2, text='Clean', command=lambda: clean_func(e2,e1))
+    b2.pack(side="left", fill="both", expand=True)
+    b3 = tk.Button(frame3, text='Quit', command=lambda: quit_func(b3))
+    b3.pack(side="bottom", fill="none", expand=True, anchor="center")
+    
+    root.resizable(False, False)
+    root.mainloop()
+
+'''
+root = tk.Tk()
+root.title('Reply - Clean - Quit')
+#root.grid_columnconfigure(0, weight=1)  # Optional for horizontal centering
+#root.grid_rowconfigure(0, weight=1)   # Optional for vertical centering
+#root.geometry("800x600")
+
+e1 = tk.Entry(root)
+e2 = tk.Entry(root)
+e1.grid(row=0, column=0,padx=10, pady=5)
+e2.grid(row=0, column=1,padx=10, pady=5)
+#e1.grid(row=0, column=0)
+#e2.grid(row=0, column=1)
+
+b1 = tk.Button(root, text='Reply', command=reply_func).grid(row=1, column=0, padx=10, pady=5)
+    
+tk.Button(root, text='Clean', command=clean_func).grid(row=1, column=1, padx=10, pady=5)
+tk.Button(root, text='Quit', command=root.quit).grid(row=2, column=0, columnspan=2)
+
+root.grid_columnconfigure(0, weight=1)  # Optional for horizontal centering
+root.grid_rowconfigure(0, weight=1)   # Optional for vertical centering
+
+root.mainloop()
+'''
+
+if __name__ == '__main__':
+    main()
+
